1d507145edbffec98cd3a9ddc207076b3776b65a
alexhong <alexhong@users.noreply.github.com>
2018-02-06 01:27:10 -0800

    script fix

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-	if (location.host) document.querySelectorAll('a.index')[0].setAttribute('href','../');
+	var a = document.querySelector('a.index');
+	if (a && location.host) a.setAttribute('href','../');
 	Array.from(document.querySelectorAll('[title]')).map(function(a){
 		var l = a.getAttribute('title');
 		a.className = l>=5000 ? 'a' : l>=1000 ? 'b' : l>=500 ? 'c' : l>=100 ? 'd' : '';
