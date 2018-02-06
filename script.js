82ecbe33522af8b0c0d92a4f2d6e7988420b34ef
alexhong <alexhong@users.noreply.github.com>
2018-02-06 01:20:22 -0800

    strip index.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+	if (location.host) document.querySelectorAll('a.index')[0].setAttribute('href','../');
 	Array.from(document.querySelectorAll('[title]')).map(function(a){
 		var l = a.getAttribute('title');
 		a.className = l>=5000 ? 'a' : l>=1000 ? 'b' : l>=500 ? 'c' : l>=100 ? 'd' : '';
