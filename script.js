2d3ee011a1652d68b0ddf64836fd689947b8c11d
alexhong <alexhong@users.noreply.github.com>
2018-02-07 12:31:42 -0800

    added search box

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function(){
+	var s = document.getElementById("search");
+	s.addEventListener("search", function(e) {
+    	if (s.value) window.location = 'https://github.com/search?type=Code&q=user:pch4t+'+encodeURIComponent(s.value);
+	});
 	Array.from(document.querySelectorAll('a')).map(function(a){
 		var l = a.getAttribute('title');
 		if (l) a.className = l>=5000 ? 'a' : l>=1000 ? 'b' : l>=500 ? 'c' : l>=100 ? 'd' : '';
